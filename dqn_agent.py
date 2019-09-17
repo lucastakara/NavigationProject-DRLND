5e2830d6cf494899bdfbda033a9aa6b5109730f6
Lucas Takara <lucastakara2@gmail.com>
2019-09-17 00:05:10 -0300

    Start class Agent and its __init__, step methods

--- dqn_agent.py
+++ dqn_agent.py
@@ -21,3 +21,43 @@ UPDATE_EVERY = 4 # how often to update the network
 
 device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu') 
 
+class Agent():
+    """ Interacts with and learns from the environment """
+    
+    def __init__(self, state_size, action_size, seed):
+        """ Initialize an Agent object.
+        
+        Params
+        ======
+        state_size(int): Dimension of each state
+        action_size(int): Dimension of each action
+        seed(int): Random seed
+        
+        """
+        self.state_size = state_size
+        self.action_size = action_size
+        self.seed = random.random.seed(seed)
+        
+        # Q - Network
+        self.qnetwork_local = QNetwork(state_size, action_size, seed).to(device)
+        self.qnetwork_target = QNetwork(state_size, action_size, seed).to(device)
+        self.optimizer = optim.Adam(self.qnetwork_local.parameters(), lr = LR)
+        
+        # Replay memory
+        self.memory = ReplayBuffer(action_size, BUFFER_SIZE, BATCH_SIZE, seed)
+        # Initialize time step (for updating every UPDATE_EVERY STEPS)
+        self.t_step = 0
+        
+        
+    def step(self, state, action, reward, next_state, done):
+        # Save experience in replay memory
+        self.memory.add(state, action, reward, next_state, done)
+        # Learn every UPDATE_EVERY time steps
+        self.t_step = (self.t_step + 1) % UPDATE_EVERY
+        if t_step == 0:
+            # If enough samples are available in memory, get random subset and learn
+            if len(self.memory) > BATCH_SIZE:
+                experiences = self.memory.sample()
+                self.learn(experiences, GAMMA)
+        
+        
